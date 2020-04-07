b61518cc6d6ebc454d12b3c54ef4b6085ff4d682
Clara Moreira <up201806309@fe.up.pt>
2020-04-07 15:43:30 +0100

    little update

--- Projeto 1/src/simpledu.c
+++ Projeto 1/src/simpledu.c
@@ -73,7 +73,7 @@ int main(int argc, char *argv[], char *envp[])
     if(isDirectory(args.path))
     {
         printf("Entrou no diretório\n");
-        getDirectoryInfo(args.path, args.max_depth, args);
+
     }
 
     return 0;
