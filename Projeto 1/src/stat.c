0674be9a2dc1ab62558a25a3b155a3a76e88cd2a
Gustavo Mendes <gustavo.sm00@gmail.com>
2020-04-07 19:16:42 +0100

    Removed dead code again

--- Projeto 1/src/stat.c
+++ Projeto 1/src/stat.c
@@ -2,54 +2,6 @@
 
 
 static int block_size; 
-int getInfo(char * pathname) //Usar isto para symbolic links se -L for passada como argumento e para ficheiros
-{
-    struct stat info;
-    if(lstat(pathname, &info)<0)
-    {
-        printf("Error\n");
-        exit(1);
-    }
-    
-    
-    int n_bytes = info.st_size;
-    int size = n_bytes/block_size;
-    if (n_bytes%block_size != 0)
-        size++;
-    
-    printResult(size,pathname);
-    return size;
-    
-}
-
-
-bool isSymbolicLink(char * pathname)
-{
-    struct stat info;
-    if(lstat(pathname, &info)<0)
-    {
-        perror("Error\n");
-        exit(1);
-    }
-    return S_ISLNK(info.st_mode);
-}
-
-int getSymbolicLinkInfo(char * pathname) 
-{
-    struct stat info;
-    if(lstat(pathname, &info)<0)
-    {
-        perror("Error\n");
-        exit(1);
-    }
-    
-    
-    int n_bytes = info.st_size;
-    int size = n_bytes/block_size;
-    printResult(size,pathname);
-    return size;
-    
-}
 
 bool isDirectory(char * pathname)
 {
@@ -60,63 +12,33 @@ bool isDirectory(char * pathname)
         exit(1);
     }
     return S_ISDIR(info.st_mode);
-
-
 }
-bool isFile(char * pathname)
-{
-    struct stat info;
-    if(lstat(pathname, &info)<0)
-    {
-        perror("Error\n");
-        exit(1);
-    }
-    return S_ISREG(info.st_mode);
-     
-}
-
 
 int callRightFunction(char * pathname, Args arg)
 {
 
 
-
-    /*if(isFile(pathname))
-    {
-        return getInfo(pathname);
-    }
-    else if(isSymbolicLink(pathname))
-    {
-        printf ("esta aqui\n");
-        if (arg.dereference) //se o argumento tem -L
-            return getInfo(pathname);
-        else
-        {
-            return getSymbolicLinkInfo(pathname);
-           
-        }
-        
-    }*/
-
     if (arg.dereference){
         struct stat info;
-        if(stat(pathname, &info)<0)
+        if(lstat(pathname, &info)<0)
         {
             printf("Error\n");
             exit(1);
         }
-        int n_bytes;
         int size;
-        if(S_ISLNK(info.st_mode)){
-            n_bytes = info.st_size;
-            size = n_bytes/block_size; 
-        }
-        else{
-            n_bytes = info.st_size;
-            size = n_bytes/block_size;
-            if (n_bytes%block_size != 0)
+        if(!S_ISLNK(info.st_mode)){
+            if(stat(pathname, &info)<0)
+            {
+                printf("Error\n");
+                exit(1);
+            }
+            size = info.st_size/block_size;
+            if (info.st_size%block_size != 0)
                 size++;
         }
+        else
+            size = info.st_size/block_size;
+        
         printResult(size,pathname);
         return size;
     }
@@ -128,9 +50,15 @@ int callRightFunction(char * pathname, Args arg)
             exit(1);
         }
         
+        int size;
+        if(S_ISLNK(info.st_mode))
+            size = info.st_size/block_size; 
         
-        int n_bytes = info.st_size;
-        int size = n_bytes/block_size;
+        else{
+            size = info.st_size/block_size;
+            if (info.st_size%block_size != 0)
+                size++;
+        }
         
         
         printResult(size,pathname);
@@ -156,7 +84,6 @@ int max_depth, Args arg)
         pid_t pid;
         DIR * newDir = opendir(pathname); //apontador para os conteudos da pasta
         struct dirent *dp;
-        //printf("Teste 1\n");
        
         char **new_files = malloc(200*sizeof(char*));
         
@@ -175,7 +102,7 @@ int max_depth, Args arg)
             }
         }
         closedir(newDir);
-        //printf("Teste 2\n");
+    
 
         char **new_paths = malloc(idx*sizeof(char*));
         for (int i = 0; i < idx; i++)
@@ -193,12 +120,9 @@ int max_depth, Args arg)
             strcpy(tmp_path, newPathname);
             strcat(tmp_path, new_files[i]);
             strcpy(new_paths[i], tmp_path);
-            //printf(new_paths[i]);
-            //printf("\n");
-            //strcpy(tmp_path, newPathname);
         }
 
-        //printf("Teste 3\n");
+    
 
         for(int i = 0; i < idx; i++)
         {
@@ -224,42 +148,19 @@ int max_depth, Args arg)
                 {
 
                     close(fd[WRITE]);
-                   // printf("Teste 4 \n");
-                    //pid_t wtp;
                     int status = 0;
                     waitpid(-1, &status, 0);
-                        
-                    //termina aqui o processo pai
-                   
-                    int s;
-                    int temp=0;
+ 
+                    int s;         
                     if(!arg.sep_dirs){
                         read(fd[READ],&s,sizeof(int));
-                        
-                        temp+=s;
                         sum+=s;
                     }
-                    //printResult(temp,newPathname);
-                    
-
-                    //exit(0);
 
                 }
                 else //processo filho
                 {
                     
-                    //int n;
-                    //read(fd[READ], &n, sizeof(int));
-                    //getDirectoryInfo(newPathname,max_depth-1);
-                    // char **commands = get_cmd_args(arg);
-                    // strcpy(commands[0], new_paths[i]);
-                    // printf("Teste 6\n");
-                    // execvp("simpledu", commands);
-                    // printf("Teste 7\n");
-                    /*Args new_args = arg;
-                    new_args.max_depth--;*/
-                    //strcpy(new_args.path, new_paths[i]);
-
                     close(fd[READ]);
                     int size =getDirectoryInfo(new_paths[i], max_depth-1,arg);
                     
@@ -270,13 +171,8 @@ int max_depth, Args arg)
                 }
             }
             else
-            {
-                int s=callRightFunction(new_paths[i], arg);
-                //if(!arg.sep_dirs)
-                    sum+=s;
-                
-                
-            }
+                sum+=callRightFunction(new_paths[i], arg);
+         
         }
 
         for (int i=0; i<idx;i++){
