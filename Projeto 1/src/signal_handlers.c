48f0f9d60e981e870a7419107e928dc14891a2fc
Leonor Gomes <leonor.email@gmail.com>
2020-04-09 15:53:26 +0100

    continuing signals

--- Projeto 1/src/signal_handlers.c
+++ Projeto 1/src/signal_handlers.c
@@ -4,15 +4,24 @@
 
 void sigint_handler(int signo)
 {
-
+    pid_t pid = getpid();
+    char answer[2];
+    write(STDOUT_FILENO, "Are you sure you want to exit? [Y/N]\n ", 39);
+    read(STDIN_FILENO, answer, 2*sizeof(char));
+    if(strcmp(answer, "N") == 0 || strcmp(answer, "n") == 0)
+    {
+        
+    }
+    
 }
 
 void sigcont_handler(int signo)
 {
-
+    write(STDOUT_FILENO, "Continuing processes...\n", 25);
 }
 
 void sigterm_handler(int signo)
 {
-
+    write(STDOUT_FILENO, "Terminating processes...\n", 26);
+    logExit(0); //não sei qual é suposto ser o código de terminação
 }
