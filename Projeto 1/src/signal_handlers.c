1ddd85bc8dcd50aadf2da69ac79d40f0427f707a
Leonor Gomes <leonor.email@gmail.com>
2020-04-09 15:36:00 +0100

    signal handlers files

---
+++ Projeto 1/src/signal_handlers.c
@@ -0,0 +1,18 @@
+#include "signal_handlers.h"
+
+
+
+void sigint_handler(int signo)
+{
+
+}
+
+void sigcont_handler(int signo)
+{
+
+}
+
+void sigterm_handler(int signo)
+{
+
+}
---
+++ Projeto 1/src/signal_handlers.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <stdio.h> 
+#include <signal.h> 
+#include <unistd.h> 
+#include <stdlib.h> 
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <string.h>
+#include <signal.h>
+
+void sigint_handler(int signo);
+
+void sigcont_handler(int signo);
+
+void sigterm_handler(int signo);
