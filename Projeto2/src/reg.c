93231d24748dec8d7239128ecf1a7008f593744e
Gustavo Mendes <gustavo.sm00@gmail.com>
2020-04-18 17:36:28 +0100

    Added makefile

---
+++ Projeto2/src/reg.c
@@ -0,0 +1,100 @@
+#include "reg.h"
+
+
+FILE *logs;
+
+void initLogs(){
+
+    setenv("LOG_FILENAME","logs.txt",0);
+
+    if((logs = fopen(getenv("LOG_FILENAME"),"w"))==NULL){
+        perror("Unable to open file");
+        exit(1);
+    }
+}
+
+
+void createReg(Reg * reg,char *action){
+    
+
+    reg->pid=getpid();
+    strcpy(reg->action, action);
+    strcpy(reg->info,"Command: ");
+}
+
+void writeReg(Reg * reg){
+    fprintf(logs,"%.2f - %.8d - %s - %s\n", time(), reg->pid, reg->action, reg->info);
+    setbuf(logs, NULL); //limpar o buffer
+}
+
+void logCreate(int argc, char *args[]) {
+    Reg reg; 
+    createReg(&reg, "CREATE");
+    for(int i = 0; i < argc; i++){
+		strcat(reg.info, args[i]);
+        if (i != argc-1)
+            strcat(reg.info," ");
+	}
+    writeReg(&reg);
+}
+
+void logExit(int status) {
+    Reg reg; 
+    createReg(&reg, "EXIT");
+    sprintf(reg.info, "%d", status);
+    writeReg(&reg);
+    exit(status);
+}
+
+void logRecvSig(char * sig) {
+    Reg reg; 
+    createReg(&reg, "RECV_SIGNAL");
+    sprintf(reg.info, "%s", sig);
+    writeReg(&reg);
+}
+
+void logSendSig(pid_t pid,char *sig) {
+    Reg reg; 
+    createReg(&reg, "SEND_SIGNAL");
+    sprintf(reg.info, "Sent %s to process %d", sig, pid);
+    writeReg(&reg);
+}
+
+
+void logRecvPipe(char* info) {
+    Reg reg; 
+    createReg(&reg, "RECV_PIPE");
+    strcat(reg.info,info);
+    writeReg(&reg);
+    
+}
+
+void logSendPipe(char* message) {
+    Reg reg; 
+    createReg(&reg, "SEND_PIPE");
+    strcat(reg.info,message);
+    writeReg(&reg);
+}
+
+void logEntry(int size, char* path) {
+    Reg reg; 
+    createReg(&reg, "ENTRY");
+    sprintf(reg.info, "%d %s", size, path);
+
+    writeReg(&reg);
+}
+
+void logEntry(int size, char* path) {
+    Reg reg; 
+    createReg(&reg, "ENTRY");
+    sprintf(reg->info, "%d %s", size, path);
+
+    writeReg(&reg);
+}
+
+	strcat(reg.info, new_path);
+    strcat(reg.info, "-l ");
+	strcat(reg.info, argline(arg));
+	writeReg(&reg);
+}
+
---
+++ Projeto2/src/reg.h
@@ -0,0 +1,33 @@
+#pragma once
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/types.h>
+#include <inttypes.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdint.h>
+#include "args.h"
+
+
+typedef struct {
+    char info[256];
+    char action[64];
+    pid_t pid;
+} Reg;
+
+void initLogs();
+void createReg(Reg * reg,char action[]);
+void writeReg(Reg * reg);
+void logCreate(int argc, char *argv[]);
+void logExit(int status);
+void logRecvSig(char * sig);
+void logSendSig(pid_t pid,char * sig);
+void logRecvPipe(char* info);
+void logSendPipe(char* message);
+void logEntry(int size, char *path);
+void logCreateFork(Args arg, int max_depth, char *new_path);
