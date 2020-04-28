a91da08422a0e54614e59bf0849f8d4f2f9171f0
Clara Moreira <up201806309@fe.up.pt>
2020-04-28 15:56:16 +0100

    added files

---
+++ Projeto2/src/Qn.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <ctype.h>
+#include <pthread.h>
+#include "args.h"
+#include "utils.h"
+#include "reg.h"
+
+
+char public_fifo[20];
+int current_time;
+int max_time;
+int order;
+
+void * dealRequest(void * arg) {
+
+    int fd;
+    Request request = *(Request *) arg;
+    char private_fifo[50];
+    fifo_name(request.pid, request.tid, private_fifo);
+    request.pid = getpid();
+    request.tid = pthread_self();
+
+    if (current_time < max_time)
+    {
+        order++;
+        request.pl = order;
+        current_time += request.dur;
+        usleep(request.dur * 1000);
+    }
+
+    do
+    {
+        fd = open(private_fifo, O_WRONLY);
+    } while (fd == -1);
+
+    write(fd, &request, sizeof(request));
+    close(fd);
+    
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    Args_qn arg = process_args_qn(argc, argv);
+    max_time = arg.nsecs;
+    strcpy(public_fifo, arg.fifoname);
+    int fd1;
+    order = 0;
+    current_time = 0;
+    
+    
+    Request request;
+    mkfifo(public_fifo, 0660);
+    fd1 = open(public_fifo, 0660);
+    while (read(fd1, &request, sizeof(request))>0)
+    {
+        pthread_t tid;
+        pthread_create(&tid, NULL, dealRequest, (void *)&request);
+        //pthread_join(tid, NULL); //acho q n devemos fazer isto
+    } 
+    logWant(request);
+    close(fd1);
+    unlink(public_fifo);
+
+    pthread_exit(0);
+   
+}
---
+++ Projeto2/src/Un.c
@@ -0,0 +1,76 @@
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <ctype.h>
+#include <pthread.h>
+#include "args.h"
+#include "utils.h"
+#include "reg.h"
+
+
+char public_fifo[20];
+int fd;
+
+void * sendRequest(void * arg) // arg vai ser número sequencial do pedido
+{
+    int fd2;
+    time_t t;
+    srand((unsigned) time(&t));
+    Request request;
+    request.pid = getpid();
+    request.i = *(int *) arg;
+    request.tid = pthread_self();
+    request.pl = -1;
+    request.dur = (rand() % 410001) + 10000;
+
+
+	logWant(request);
+
+
+	write(fd, &request, sizeof(request));
+
+	char fifo[50];
+    fifo_name(request.pid, request.tid, fifo);
+    mkfifo(fifo, 0660);
+
+    fd2 = open(fifo, O_RDONLY);
+
+    //Tratar da parte de dar display da informação
+
+    close(fd2);
+    unlink(fifo);
+
+}
+
+
+int main(int argc, char *argv[])
+{
+
+    Args_un arg = process_args_un(argc, argv);
+    strcpy(public_fifo, arg.fifoname);
+    
+    int i = 1;
+    int current_time = 0;
+    do{
+        fd = open(public_fifo, O_WRONLY);
+    } while(fd == -1);
+
+    while(current_time < arg.nsecs)
+    {
+        pthread_t tid;
+        pthread_create(&tid, NULL, sendRequest, (void *)&i);
+        i++;
+        current_time+=2;
+        usleep(2*1000000);
+    }
+
+    close(fd);
+
+    pthread_exit(0);
+
+}
